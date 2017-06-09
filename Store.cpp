f064484c12a56be08d6274b66c2d76d491460a74
Drot88 <Daniel.Rotenberg88@gmail.com>
2017-06-08 23:36:18 -0400

    Added products and members

--- Store.cpp
+++ Store.cpp
@@ -54,16 +54,51 @@ void Store::checkOutMember(string mID) {
 
 
 int main() {
-  Product* prod;
-  Store testStore;
-  Product newp("10000002", "Cheese Whiz", "Great on crackers!", 4.99, 10);
-  prod = &newp;
+  Store newStore;
+  Product* prodToAdd;
+  Customer* custToAdd;
+
+  Product newp("00001", "Cheese Whiz", "Great on crackers!", 4.99, 10);
+  prodToAdd = &newp;
+  newStore.addProduct(prodToAdd);
+
+  Product newp2("00002", "Frosted Flakes", "Tony the Tiger's favorite cereal.", 3.99, 20);
+  prodToAdd = &newp2;
+  newStore.addProduct(prodToAdd);
+
+  Product newp3("00003", "Cat Scratcher", "For your furball friends", 63.49, 5);
+  prodToAdd = &newp3;
+  newStore.addProduct(prodToAdd);
+
+  Product newp4("00004", "Honey", "To help sweeten Tea.", 4.99, 10);
+  prodToAdd = &newp4;
+  newStore.addProduct(prodToAdd);
+
+  Product newp5("00005", "Samsung 55' TV", "Great for viewing your favorite sunday night TV shows.", 1100.00, 17);
+  prodToAdd = &newp5;
+  newStore.addProduct(prodToAdd);
+
+  Product newp6("00003", "Cat Brush", "Brush to reduce shedding.", 3.49, 12);
+  prodToAdd = &newp6;
+  newStore.addProduct(prodToAdd); 
+
+
+  Customer newCust("Clara", "70000001", true);
+  custToAdd = &newCust;
+  newStore.addMember(custToAdd);
+
+  Customer newCust2("Daniel", "70000002", true);
+  custToAdd = &newCust2;
+  newStore.addMember(custToAdd);
+
+  Customer newCust3("Jesse", "70000003", false);
+  custToAdd = &newCust3;
+  newStore.addMember(custToAdd);  
+
 
   cout << endl << "In Main" << endl;
-  Store newStore;
-  newStore.addProduct(prod);
-  cout << prod << endl;
-  cout << newStore.getProductFromID("10000002") << endl;
+  cout << "Location of Product 00001: " << newStore.getProductFromID("00001") << endl;
+  cout << "Location of Member 700000001: " << newStore.getMemberFromID("70000001") << endl;
 
   return 0;
 }
